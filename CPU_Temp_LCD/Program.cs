993cf035d08e0e70bbc85948100709e8b96b9fd4
Piotr Redmerski <Piotr Redmerski>
2017-04-01 13:24:22 +0200

    Add project files.

---
+++ CPU_Temp_LCD/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPU_Temp_LCD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
